b4a140885fbca4282e3bd5709d0c4b6a0f3cafe5
usefulsplash <emjavier16@gmail.com>
2022-10-12 18:31:44 -0400

    custom colors added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2E2C25",
+        secondary: "#BFC89E",
+        accent: "#4A5650",
+      },
+    },
+  },
+  plugins: [],
+};
